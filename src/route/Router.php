f73161b5d6d2c60181d26f9b3111f5db57a86364
kus <kusjoybera@gmail.com>
2020-02-25 18:33:33 +0530

    fixed bug in routing

--- src/route/Router.php
+++ src/route/Router.php
@@ -16,10 +16,14 @@ class Router
 
     public function dispatch()
     {
-        $path = $_SERVER['PATH_INFO'];
-        if(array_key_exists($path, $this->routes)) {
-            $this->routes[$path]();
-        } else {
+        if( array_key_exists('PATH_INFO',$_SERVER) )
+        {
+            $path = $_SERVER['PATH_INFO'];
+            if(array_key_exists($path, $this->routes)) {
+                $this->routes[$path]();
+            } 
+        }
+        else {
             $this->routes['/']();
         }
     }
--- src/views/product.php
+++ src/views/product.php
@@ -1,25 +1,17 @@
 <html>
     <head>
         <title>
-            product form
+            product name
         </title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="/assets/css/pro.css">
     </head>
     <body>
-        <?php
-
-        // echo '<pre>';
-        // print_r($data);
-        // echo '</pre>';
-        // die();
-        ?>
-
         <!-- showing all product data -->
         <div class="container mt-5">
             <div class="row">
                 <div class="col-sm-9 product-container">
-                    <h5>Product List</h5>
+                    <h5>Product Name</h5>
 
                     <?php if( !empty( $data ) ) : ?>
                     <table class="table">
